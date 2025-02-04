85724b36cd5c671ebce3ec3543d8e3df3c2c6ec8
ShavindiSanthupamaLiyanage <shavis1113@gmail.com>
2025-02-04 18:36:56 +0530

    Initial Commit

---
+++ scripts/navigation.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("../templates/header.html")
+        .then(response => response.text())
+        .then(data => document.getElementById("header").innerHTML = data);
+
+    fetch("../templates/footer.html")
+        .then(response => response.text())
+        .then(data => document.getElementById("footer").innerHTML = data);
+});
+
